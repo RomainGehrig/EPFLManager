e6dd71f4a155f121e6f6f4e1ce4155649d8828ad
Romain Gehrig <romain.gehrig@gmail.com>
2016-03-15 19:09:30 +0100

    Configuration is now handled as a component

--- epflmanager/config.py
+++ epflmanager/config.py
@@ -1,9 +1,9 @@
 import epflmanager.components as components
 
-class _Config(components.Component):
+class Config(components.Component):
     def __init__(self):
         self.configuration = {}
-        super(_Config, self).__init__("Config")
+        super(Config, self).__init__("Config")
 
     def add(self, key, value):
         if key in self.configuration:
@@ -12,7 +12,3 @@ class _Config(components.Component):
 
     def get(self, key):
         return self.configuration[key]
-
-_config = _Config()
-add = _config.add
-get = _config.get
