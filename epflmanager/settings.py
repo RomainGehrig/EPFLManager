51bb74d3058e255e0825dcc464b5a535366bd12b
Romain Gehrig <romain.gehrig@gmail.com>
2016-03-15 20:38:26 +0100

    Remove unused code

--- epflmanager/settings.py
+++
@@ -1,8 +0,0 @@
-import os
-
-KNOWLEDGE_BASE_FILE = "kb.dat"
-
-LOGS_DIR = "logs"
-DEBUG_LOG_FILE = os.path.join(LOGS_DIR, "debug.log")
-MAIN_LOG_FILE = os.path.join(LOGS_DIR, "main.log")
-DEFAULT_LOG_LEVEL = "DEBUG"
--- epflmanager/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .alias_list import AliasList
-from .alias_map import AliasMap
-from .log import get_logger
