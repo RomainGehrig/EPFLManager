a069516d40da067c459b67a69c3757f5a7beb9b1
Romain Gehrig <romain.gehrig@gmail.com>
2016-03-23 23:21:32 +0100

    Correct bug induced by not having get_file anymore
    Would have been caught with some unittest I guess... hem

--- epflmanager/commands/schedule.py
+++ epflmanager/commands/schedule.py
@@ -1,11 +1,12 @@
-from epflmanager.common import *
+import os
 
+from epflmanager.common import *
 import epflmanager.components as components
 
 class Schedule(object):
     @staticmethod
     def show(args):
         schedule_file = components.get("Config")["directories"]["schedule_file"]
-        schedule = args.semester.get_file(schedule_file)
-        if schedule:
-            display_img(schedule.fullpath())
+        path = os.path.join(args.semester.fullpath(), schedule_file)
+        if os.path.exists(path):
+            display_img(path)
